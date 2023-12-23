f62b7ffa33660cf09b012ed53c48fbfba762ebaf
ROGER OPORTA <roportaperez@gmail.com>
2023-12-23 15:52:02 -0600

    Annadiendo validaciones al callback

--- 04-advanced/source/concepts/02-callbacks.js
+++ 04-advanced/source/concepts/02-callbacks.js
@@ -1,34 +1,40 @@
-import {heroes} from '../data/heroes'
+import { heroes } from '../data/heroes'
 
 /**
  * 
  * @param {HTMLDivElement} element 
  */
 export const callbacksComponent = (element) => {
-    
-    //console.log('callbacksComponent');
 
-    const id = '5d86371f1efebc31def272e2';
-    findHero(id, (hero) => {
+    const id = '5d86371f233dc9f2425f16916';
+    findHero(id, (error, hero) => {
 
         //aqui se esta haciendo la logica del callback
+         if (error) {
+             element.innerHTML = error;
+             console.log(error);
+             return;
+         }
         element.innerHTML = hero.name;
-        console.log(hero.name);
-        
     });
-    
- }
 
- /**
-  * Ejemplo de funcion callback:
-  * callback, es una funcion que  recibe un argumento y ese argumento es una 
-  * funcion que a la vez se envoca de la funcion padre definida
-  * @param {String} id 
-  * @param {(hero: Object)=> void} callback 
-  */
- const findHero = (id , callback ) => {
+}
+
+/**
+ * Ejemplo de funcion callback:
+ * callback, es una funcion que  recibe un argumento y ese argumento es una 
+ * funcion que a la vez se envoca de la funcion padre definida
+ * @param {String} id 
+ * @param {(error: String|null,hero: Object)=> void} callback 
+ */
+const findHero = (id, callback) => {
 
     const hero = heroes.find(hero => hero.id === id)
 
-    callback(hero)
- }
+    if (!hero) {
+        callback(`NO HEROE FOUNDED with ID=${id}`);
+        return;
+    }
+
+    callback(null,hero);
+}
